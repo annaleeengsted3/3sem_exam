1ba0d1505600a33602da9542a783b7b4db6361f0
Annalee Engsted <annalee.engsted@hotmail.com>
2019-12-01 16:04:48 +0100

    separated game w sprites and game w 1 container-svg

--- gamewsprite.js
+++ gamewsprite.js
@@ -1,56 +1,49 @@
 "use strict";
-console.log("runnin");
+
 window.addEventListener("DOMContentLoaded", init);
 
 let time;
 let gameover = false;
-//const fences = document.querySelectorAll(".fence circle");
-// const fences = document.querySelectorAll(".fence");
-const fences = document.querySelectorAll(".fence circle");
+const fences = document.querySelectorAll(".fence");
+const fencesHitbox = document.querySelectorAll(".fence circle");
 const horse = document.querySelector("#horse");
 //const fence = document.querySelectorAll(".fence");
 
 function init() {
   document.querySelector(`button[data-action="start"]`).addEventListener("click", () => {
-    isCollided();
     startAnimations();
     timeCount();
+    document.onkeydown = checkKey;
+    document.onkeyup = resetAnimation;
   });
   document.querySelector(`button[data-action="stop"]`).addEventListener("click", () => {
     gameover = true;
     stopCount();
   });
   //moveFence();
+  isCollided();
 }
 
 function startAnimations() {
   fences.forEach(fence => {
     fence.classList.add("fence_move");
   });
+  document.querySelector(".second_fence").addEventListener("animationiteration", calculateDelay);
 }
 
-// const speed = 19; //vw per s
-// let last;
-// let pos = 0;
+function calculateDelay() {
+  console.log("delay calculating");
+  let number = generateRandomNumber();
+  console.log(number);
+  //document.querySelector(".second_fence").style.animationDelay = `${number}s`;
+  document.querySelector(".second_fence").style.transform = `translateX(${number}vw)`;
+  //document.querySelector(".second_fence").style.setProperty("--delay", number);
+}
 
-// function moveFence() {
-//   const now = performance.now();
-
-//   const delta = now - last || 0;
-
-//   last = now;
-
-//   pos += speed / (1000 / delta);
-//   if (pos > 110) {
-//     pos = 0;
-//   }
-//   fence.style.transform = `translateX(-${pos}vw)`;
-
-//   requestAnimationFrame(moveFence);
-// }
-
-document.onkeydown = checkKey;
-document.onkeyup = resetAnimation;
+function generateRandomNumber() {
+  //generate a random second delay (or translateX value?) for fence #2, between 1.5s and 3s
+  return Math.random() * (15 - 12 + 1) + 12;
+}
 
 let xPos = 0;
 let yPos = 150;
@@ -73,6 +66,9 @@ function checkKey(e) {
     //space
     //keys.space = true;
     horse.classList.add("jump");
+    // horse.addEventListener("animationend", ()=>{
+    //   horse.classList.remove("jump");
+    // })
   }
 }
 
@@ -80,20 +76,20 @@ function resetAnimation() {
   horse.classList.remove("jump");
 }
 
-const horsePos = document.querySelector("#horse circle").getBoundingClientRect();
-
 function isCollided() {
   //const horsePos = document.querySelector("#horse circle");
+  const horsePos = document.querySelector("#horse circle").getBoundingClientRect();
 
   //const fencePos = document.querySelector(".fence circle").getBoundingClientRect();
   let collided = false;
-  fences.forEach(fence => {
+  fencesHitbox.forEach(fence => {
     const fencePos = fence.getBoundingClientRect();
     const dx = horsePos.x - fencePos.x;
     const dy = horsePos.y - fencePos.y;
     const distance = Math.sqrt(dx * dx + dy * dy);
+    //console.log(horseY);
 
-    if (distance < horsePos.height / 2 + fencePos.height / 2) {
+    if (distance < horsePos.width / 2 + fencePos.width / 2) {
       console.log("COLLISION");
       document.querySelector("#horse circle").classList.add("collision");
       collided = true;
@@ -102,17 +98,21 @@ function isCollided() {
       document.querySelector("#horse circle").classList.remove("collision");
     }
   });
-  //   requestAnimationFrame(isCollided);
 
-  //console.log(horsePos.cy.animVal);
-  if (!collided) {
-    requestAnimationFrame(isCollided);
-  }
+  requestAnimationFrame(isCollided);
+  // if (!collided) {
+  //   requestAnimationFrame(isCollided);
+  // }
+
+  // const horseX = horsePos.right - horsePos.left;
+  // const fenceX = fencePos.right - fencePos.left;
+  // const horseY = horsePos.bottom - horsePos.top;
+  // const fenceY = fencePos.bottom - fencePos.top;
+  // const dx = horseX - fenceX;
+  // const dy = horseY - fenceY;
 }
 
 function timeCount() {
-  console.log("function time=" + time);
-
   const startTime = Date.now();
   let elapsedTime;
 
@@ -131,6 +131,6 @@ function showTime(elapsedTime) {
 }
 
 function stopCount() {
-  console.log("stopped time");
+  //console.log("stopped time");
   document.querySelector("#time").innerHTML = time;
 }
