8befaf39ad45bee20bf015d96cf3d5af0a18814f
Annalee Engsted <annalee.engsted@hotmail.com>
2019-12-02 12:27:33 +0100

    working prototype, randomised fences working now, faster time not quite

--- gamewsprite.js
+++ gamewsprite.js
@@ -5,44 +5,52 @@ window.addEventListener("DOMContentLoaded", init);
 let time;
 let gameover = false;
 const fences = document.querySelectorAll(".fence");
-const fencesHitbox = document.querySelectorAll(".fence circle");
+
 const horse = document.querySelector("#horse");
 //const fence = document.querySelectorAll(".fence");
 
 function init() {
   document.querySelector(`button[data-action="start"]`).addEventListener("click", () => {
+    document.querySelector("#popup").style.display = "none";
     startAnimations();
     timeCount();
     document.onkeydown = checkKey;
     document.onkeyup = resetAnimation;
+    isCollided();
   });
   document.querySelector(`button[data-action="stop"]`).addEventListener("click", () => {
     gameover = true;
     stopCount();
   });
   //moveFence();
-  isCollided();
 }
 
 function startAnimations() {
-  fences.forEach(fence => {
-    fence.classList.add("fence_move");
-  });
-  document.querySelector(".second_fence").addEventListener("animationiteration", calculateDelay);
+  let number = generateRandomNumber();
+
+  setTimeout(function() {
+    createFence();
+    requestAnimationFrame(startAnimations);
+  }, number);
 }
 
-function calculateDelay() {
-  console.log("delay calculating");
-  let number = generateRandomNumber();
-  console.log(number);
-  //document.querySelector(".second_fence").style.animationDelay = `${number}s`;
-  document.querySelector(".second_fence").style.transform = `translateX(${number}vw)`;
-  //document.querySelector(".second_fence").style.setProperty("--delay", number);
+function createFence() {
+  const fence = document.createElement("div");
+  fence.className = "fence";
+  document.body.appendChild(fence);
+  addAnimation(fence);
+}
+
+function addAnimation(fence) {
+  //fence.classList.add("fence_move");
+  fence.addEventListener("animationend", () => {
+    fence.remove();
+  });
 }
 
 function generateRandomNumber() {
   //generate a random second delay (or translateX value?) for fence #2, between 1.5s and 3s
-  return Math.random() * (15 - 12 + 1) + 12;
+  return Math.random() * (4000 - 1200 + 1) + 1200;
 }
 
 let xPos = 0;
@@ -77,10 +85,10 @@ function resetAnimation() {
 }
 
 function isCollided() {
+  //console.log("collision detection started");
   //const horsePos = document.querySelector("#horse circle");
+  const fencesHitbox = document.querySelectorAll(".fence");
   const horsePos = document.querySelector("#horse circle").getBoundingClientRect();
-
-  //const fencePos = document.querySelector(".fence circle").getBoundingClientRect();
   let collided = false;
   fencesHitbox.forEach(fence => {
     const fencePos = fence.getBoundingClientRect();
@@ -103,13 +111,6 @@ function isCollided() {
   // if (!collided) {
   //   requestAnimationFrame(isCollided);
   // }
-
-  // const horseX = horsePos.right - horsePos.left;
-  // const fenceX = fencePos.right - fencePos.left;
-  // const horseY = horsePos.bottom - horsePos.top;
-  // const fenceY = fencePos.bottom - fencePos.top;
-  // const dx = horseX - fenceX;
-  // const dy = horseY - fenceY;
 }
 
 function timeCount() {
@@ -128,6 +129,11 @@ function timeCount() {
 
 function showTime(elapsedTime) {
   document.querySelector("#time").innerHTML = elapsedTime;
+  if (elapsedTime > 10) {
+    console.log("time over 10");
+    //to do: fix this! Jerking animation, probably called too many times- where else to put this?
+    document.querySelector(".fence").style.animationDuration = `2s`;
+  }
 }
 
 function stopCount() {
