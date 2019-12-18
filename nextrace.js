9af9b2b9bf1e594e6a2da78e19ad34078f0b479a
Josefine Margrethe Smidt Hansen <jose317f@stud.kea.dk>
2019-12-18 14:32:16 +0100

    ændret pattern i brugernavn så æ,ø,å er tilladt

--- nextrace.js
+++ nextrace.js
@@ -11,7 +11,7 @@ function loadJSON() {
   fetch("http://josefinemargrethe.dk/kea/3-semester/dantoto/wordpress/wp-json/wp/v2/event?per_page=100")
     .then(response => response.json())
     .then(jsonData => {
-      // when JSON is loaded, we will clone the data into our file
+      //når JSON er loadet tager vi vores data med videre til prepareData
       prepareData(jsonData);
     });
 }
@@ -19,7 +19,7 @@ function loadJSON() {
 function prepareData(jsonData) {
   races = jsonData;
 
-  // sorter races efter date_time (omdøb til race_time), med ældste først
+  // sorter races efter deres løbstidspunkt, med ældste først
   races.sort((a, b) => {
     if (b.race_time < a.race_time) {
       return 1;
@@ -28,19 +28,20 @@ function prepareData(jsonData) {
     }
   });
 
-  // gå igennem races, check om date_time er før now,
+  // gå igennem races, check om race_time er før now,
   const now = new Date();
   for (let i = 0; i < races.length; i++) {
     const race = races[i];
     const race_time = new Date(race.race_time);
     console.log(race);
 
+    // hvis ikke, så har vi fundet next Race - og stop! med break;
+
     if (race_time.getTime() > now.getTime()) {
       nextRace = race;
       break;
     }
   }
-  // hvis ikke, så har vi fundet next Race - og stop! med break;
 
   console.log("næste løb er " + nextRace.race_time);
 
@@ -48,38 +49,35 @@ function prepareData(jsonData) {
 }
 
 function showCountdown(nextRace) {
-  // Set the date we're counting down to
+  // sætter countdown dato til at være næste løb - som vi har fundet ovenfor
 
   let countDownDate = new Date(nextRace.race_time).getTime();
 
-  // Update the count down every 1 second
+  // vi bruger setInterval til at tælle ned hvert sekund
   let x = setInterval(function() {
-    // Get today's date and time
+    //finder dato og tid lige nu
     let now = new Date().getTime();
 
-    // Find the distance between now and the count down date
+    // vi finder distancen mellem nu og countdown-datoen
     let distance = countDownDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
+    // her laver vi beregninger på den 'distance' vi lige har fundet, så vi kan vise nedtællingen i både dage, timer, minutter og sekunder
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display the result in the element with id="countdown"
+    // Vi laver et loop og indsætter nedtællingen samt stedet på pågældende løb i vores HTML
     document.querySelectorAll(".countdown").forEach(div => {
       div.innerHTML = days + "d " + hours + "t " + minutes + "m " + seconds + "s ";
     });
     document.querySelectorAll(".location").forEach(div => {
       div.innerHTML = `${nextRace.location}`;
     });
-    // document.querySelector("#countdown").innerHTML = days + "d " + hours + "t " + minutes + "m " + seconds + "s ";
-    // document.querySelector("#location").innerHTML = `${nextRace.location}`;
 
-    // If the count down is finished, write "UDLØBET" text
+    // når distancen er mindre end 0 kalder vi loadJSON igen, så den automatisk læser dataen igen for at finde næste løb i rækken
     if (distance < 0) {
       clearInterval(x);
-      //document.querySelector("#countdown").innerHTML = "LIVE NU";
       loadJSON();
     }
   }, 1000);
