dcfb60f4c5ce51dc796aa9948621142eaff95e2c
Annalee Engsted <annalee.engsted@hotmail.com>
2019-12-19 10:23:00 +0100

    fixed reloading issues

--- recentwinners.js
+++ recentwinners.js
@@ -63,29 +63,6 @@ function visSpillere(spillere) {
   }, 3000);
 }
 
-//Make a new user when clicking submit
-const form = document.querySelector("form");
-
-function submitForm() {
-  form.addEventListener("submit", evt => {
-    evt.preventDefault();
-    //   const boxes = Array.from(form.querySelectorAll("[type=checkbox]:checked"));
-    //   const nice = boxes.map(el => {
-    //     return el.value;
-    //   });
-    let userObject = {
-      brugernavn: form.elements.brugernavn.value,
-      email: form.elements.email.value,
-      password: form.elements.password.value,
-      by: form.elements.by.value,
-      terms: form.elements.terms.checked,
-      kr_vundet: 0
-    };
-    post(userObject);
-    form.reset();
-  });
-}
-
 form.elements.brugernavn.addEventListener("blur", checkForm);
 form.elements.email.addEventListener("blur", checkForm);
 // document.querySelector(".nextBtn").addEventListener("click", checkForm);
@@ -94,25 +71,13 @@ function checkForm() {
   console.log("blurred");
   allebrugere.forEach(user => {
     if (user.brugernavn == form.elements.brugernavn.value || user.email == form.elements.email.value) {
-      alert("bruger eksisterer allerede");
+      //alert("bruger eksisterer allerede");
       //disable "videre" knappen?
+      document.querySelector(".informer").textContent = "Eksisterer allerede";
+      document.querySelector(".informer").style.color = "red";
+      document.querySelector(".nextBtn").style.backgroundColor = "gray";
+      document.querySelector(".nextBtn").disabled = true;
+    } else {
     }
   });
-}
-
-function post(data) {
-  const postData = JSON.stringify(data);
-  fetch("https://dantoto-1f71.restdb.io/rest/brugere", {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      "x-apikey": "5dea0bc34658275ac9dc23ad",
-      "cache-control": "no-cache"
-    },
-    body: postData
-  })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-    });
 }
